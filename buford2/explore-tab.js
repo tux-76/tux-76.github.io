6a8046a7b7af4ce3974b9b387de896b0dbe12fc2
tux-76 <tux76@proton.me>
2022-11-23 18:32:57 -0600

    dev bu2

--- buford2/explore-tab.js
+++ buford2/explore-tab.js
@@ -1,26 +1,40 @@
 // -------------------------------------poplulate explore tab
 
+function makeDirElement(name) {
+    let el = document.createElement('li');
+    el.innerHTML = `<button class="collapsible">${name}</button><ul class="dropdown-list"></ul>`;
+    return el;
+}
+function makeFuncElement(name) {
+    let el = document.createElement('li');
+    el.innerHTML = `<a class="func">${name}</a>`;
+    return el;
+}
+
+function populateDir(jsonDir, appendTo) {
+    for (const key in jsonDir) {
+        let jsonItem = jsonDir[key];
+
+        if (jsonItem.type === "dir") {
+            let newEl = makeDirElement(jsonItem.name);
+            populateDir(jsonItem, newEl.children[1]);
+
+            appendTo.appendChild(newEl);
+        } else if (jsonItem.type === "func") {
+            let newEl = makeFuncElement(jsonItem.name);
+
+            newEl.children[0].href = `./function/?func=${key}`;
+
+            appendTo.appendChild(newEl);
+        }
+    }
+}
+
 function populateExplore(indexData) {
     console.log("Index of Buford2 received:", indexData);
 
-    function makeDirElement(name) {
-        let el = document.createElement('li');
-        el.innerHTML = `<button class="collapsible">${name}</button><ul class="dropdown-list"></ul>`;
-        return el;
-    }
-    function makeFuncElement(name) {
-        let el = document.createElement('li');
-        el.innerHTML = `<a class="func">${name}</a>`;
-        return el;
-    }
-
     let explore = document.body.getElementsByClassName("main-list")[0];
-    for (const key in indexData) {
-        let item = indexData[key];
-        if (item.type === "dir") {
-            explore.appendChild(makeDirElement(item.name))
-        }
-    }
+    populateDir(indexData, explore)
 }
 
 fetch("../lib/buford2/index.json")
